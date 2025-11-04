9c1b8875f21bf1b1749422acf06620cdff07a528
builderdev212 <builder@builderdev.tech>
2025-11-03 20:30:27 -0500

    start interrupts

--- lib/adxl345/include/adxl345.h
+++ lib/adxl345/include/adxl345.h
@@ -8,6 +8,9 @@
  * I2C Status Constants *
  ************************/
 
+#define INT_PORT PORTD
+#define INT_PORT_IO_CONF DDRD
+#define ADXL345_INT_PIN 0x04
 #define ADXL345_ADDR 0x53
 #define ADXL345_ID 0xE5
 
@@ -20,6 +23,19 @@ extern "C" {
 #endif
 
 /**
+ * @brief Data structure to store acceleration data.
+ * 
+ * @param x acceleration in the x axis.
+ * @param y acceleration in the y axis.
+ * @param z acceleration in the z axis.
+ */
+struct acceleration {
+    int16_t x;
+    int16_t y;
+    int16_t z;
+};
+
+/**
  * @brief Check if the ADXL345 is on the I2C bus.
  * 
  * @return 1 if the device is found, otherwise 0.
@@ -30,6 +46,13 @@ uint8_t is_adxl345_present();
  * @brief Put the ADXL345 into measure mode and ensure it is out of sleep.
  */
 void enable_adxl345();
+
+/**
+ * @brief Get the current acceleration measured by the accelerometer.
+ * 
+ * @return acceleration struct.
+ */
+struct acceleration get_current_acceleration();
 
 #ifdef __cplusplus
 }
--- lib/adxl345/src/adxl345.c
+++ lib/adxl345/src/adxl345.c
@@ -7,6 +7,14 @@
 #define ADXL345_ID_ADDR 0x00
 #define ADXL345_POWER_CTL 0x2D
 #define ADXL345_DATA_FORMAT 0x31
+#define ADXL345_DATA_START 0x32
+#define ADXL345_BR 0x2C
+
+/***************************
+ * ADXL345 MACRO Functions *
+ ***************************/
+
+#define CONCAT_CAST_2xU8_TO_S16(val1, val2) (int16_t)(((uint16_t)val2 << 8) | (uint16_t)val1)
 
 /**************************
  * ADXL345 User Functions *
@@ -20,10 +28,28 @@ uint8_t is_adxl345_present() {
 }
 
 void enable_adxl345() {
-    // setup power control register
-    uint8_t pcrdata[2] = {ADXL345_POWER_CTL, 0x08};
-    write_i2c(ADXL345_ADDR, pcrdata, 2);
+    // setup power control(int16_t) register
+    uint8_t pcwdata[2] = {ADXL345_POWER_CTL, 0x08};
+    write_i2c(ADXL345_ADDR, pcwdata, 2);
+    // setup the device bit rate (12.5Hz)
+    uint8_t brwdata[2] = {ADXL345_BR, 0x07};
+    write_i2c(ADXL345_ADDR, brwdata, 2);
     // setup the data format
-    uint8_t dfrdata[2] = {ADXL345_DATA_FORMAT, 0x00};
-    write_i2c(ADXL345_ADDR, dfrdata, 2);
+    uint8_t dfwdata[2] = {ADXL345_DATA_FORMAT, 0x00};
+    write_i2c(ADXL345_ADDR, dfwdata, 2);
+    // setup the interrupt pin (INT0)
+    EICRA = 0x03;
+    EIMSK = 0x01;
+    INT_PORT |= ADXL345_INT_PIN;
+    INT_PORT_IO_CONF &= ~(ADXL345_INT_PIN);
+}
+
+struct acceleration get_current_acceleration() {
+    uint8_t adata[6];
+    addr_read_i2c(ADXL345_ADDR, ADXL345_DATA_START, adata, 6);
+    struct acceleration curr;
+    curr.x = CONCAT_CAST_2xU8_TO_S16(adata[1], adata[0]);
+    curr.y = CONCAT_CAST_2xU8_TO_S16(adata[3], adata[2]);
+    curr.z = CONCAT_CAST_2xU8_TO_S16(adata[5], adata[4]);
+    return curr;
 }
