193374b7d403268e9fb751594328b42609dfaab5
Builder212 <93960938+builderdev212@users.noreply.github.com>
2025-11-06 11:39:18 -0500

    add adxl345 (#6)

---
+++ lib/adxl345/src/adxl345.c
@@ -0,0 +1,47 @@
+#include "adxl345.h"
+
+uint8_t is_adxl345_present() {
+    uint8_t device_id[1] = {0x00};
+    addr_read_i2c(ADXL345_ADDR, ADXL345_ID_ADDR, device_id, 1);
+    if (device_id[0] != ADXL345_ID) return 0;
+    return 1;
+}
+
+void enable_adxl345() {
+    // setup power control(int16_t) register
+    uint8_t pcwdata[2] = {ADXL345_POWER_CTL, 0x08};
+    write_i2c(ADXL345_ADDR, pcwdata, 2);
+    // setup the device bit rate (12.5Hz)
+    uint8_t brwdata[2] = {ADXL345_BR, 0x07};
+    write_i2c(ADXL345_ADDR, brwdata, 2);
+    // setup the data format
+    uint8_t dfwdata[2] = {ADXL345_DATA_FORMAT, 0x00};
+    write_i2c(ADXL345_ADDR, dfwdata, 2);
+}
+
+void setup_adxl345_activity_interrupt() {
+    // setup the interrupt pin (INT0)
+    EICRA = 0x03;
+    EIMSK = 0x01;
+    INT_PORT |= ADXL345_INT_PIN;
+    INT_PORT_IO_CONF &= ~(ADXL345_INT_PIN);
+    // enable global interrupts
+    sei();
+    // setup activity interrupt
+    uint8_t atwdata[2] = {ADXL345_ACT_THRESH, 0x10};
+    write_i2c(ADXL345_ADDR, atwdata, 2);
+    uint8_t acwdata[2] = {ADXL345_ACT_CTRL, 0xF0};
+    write_i2c(ADXL345_ADDR, acwdata, 2);
+    uint8_t icwdata[2] = {ADXL345_INT_EN, 0x10, 0xEF};
+
+}
+
+struct acceleration get_current_acceleration() {
+    uint8_t adata[6];
+    addr_read_i2c(ADXL345_ADDR, ADXL345_DATA_START, adata, 6);
+    struct acceleration curr;
+    curr.x = CONCAT_CAST_2xU8_TO_S16(adata[1], adata[0]);
+    curr.y = CONCAT_CAST_2xU8_TO_S16(adata[3], adata[2]);
+    curr.z = CONCAT_CAST_2xU8_TO_S16(adata[5], adata[4]);
+    return curr;
+}
--- lib/atmega328p_i2c/include/atmega328p_i2c.h
+++ lib/atmega328p_i2c/include/atmega328p_i2c.h
@@ -1,7 +1,8 @@
-#ifndef ATMEGA328P_I2C
-#define ATMEGA328P_I2C
+#ifndef ATMEGA328P_I2C_H
+#define ATMEGA328P_I2C_H
 
 #include <avr/io.h>
+#include <stdint.h>
 
 /************************
  * I2C Status Constants *
@@ -13,11 +14,74 @@
 #define I2C_SCL_PIN 0x20 // pin 5
 
 #define I2C_START 0x08 // sent start
+#define I2C_REPEATED_START 0x10 // sent restart
 #define I2C_SEND_WADDR 0x18 // sent write address
 #define I2C_SEND_RADDR 0x40 // sent read address
 #define I2C_SEND_DATA 0x28 // sent data
 #define I2C_RECV_DATA 0x50 // recieved data
 #define I2C_RECV_DATA_END 0x58 // controller propagated nack to end read from target device
+
+/******************************************************************
+ * I2C Internal MACROS                                            *
+ * used by the precompiler as "copy and paste" code, removes      *
+ * unneccesary function calls while cleaning up the overall code. *
+ ******************************************************************/
+
+/**
+ * @brief Wait for the current stage of the transaction to finish.
+ */
+#define WAIT_FOR_TRANSMIT() while(!((TWCR & 0x80) >> 7))
+
+/**
+ * @brief Check the current STATUS of the I2C hardware.
+ * 
+ * @param expected_status used to check if the status in hardware matches the expected status.
+ */
+#define STATUS(expected_status) ((TWSR & 0xF8) == expected_status)
+
+/**
+ * @brief Transmit START to begin the I2C transaction.
+ */
+#define START() \
+    TWCR = 0xA4; \
+    WAIT_FOR_TRANSMIT(); \
+    if (!STATUS(I2C_START)) return
+
+#define RESTART() \
+    TWCR = 0xA4; \
+    WAIT_FOR_TRANSMIT(); \
+    if (!STATUS(I2C_REPEATED_START)) return
+
+/**
+ * @brief SEND data over the I2C bus to the target device.
+ * 
+ * @param data 8-bit value to be sent to the target.
+ * @param expected_status should be either I2C_SEND_ADDR or I2C_SEND_DATA
+ */
+#define SEND(data, expected_status) \
+    TWDR = data; \
+    TWCR = 0x84; \
+    WAIT_FOR_TRANSMIT(); \
+    if (!STATUS(expected_status)) return
+
+/**
+ * @brief RECIEVE data from the target I2C device.
+ * 
+ * @param data used to store the recieved data from the target.
+ * @param last set to 1 if this is the final byte to be read, 0 otherwise.
+ */
+#define RECV(data, last) \
+    TWCR = 0x84 | ((last) ? 0x00 : 0x40); \
+    WAIT_FOR_TRANSMIT(); \
+    if (!STATUS((last) ? I2C_RECV_DATA_END : I2C_RECV_DATA)) return; \
+    data = (uint8_t)TWDR
+
+/**
+ * @brief Transmit STOP to end I2C transaction.
+ */
+#define STOP() \
+    TWCR = 0x94; \
+    while ((TWCR & 0x10) >> 4)
 
 /*********************************
  * I2C User Function Definitions *
@@ -27,9 +91,38 @@
 extern "C" {
 #endif
 
+/**
+ * @brief Setup the I2C hardware.
+ */
 void start_i2c();
+
+/**
+ * @brief I2C write function.
+ * 
+ * @param dev_addr target device address.
+ * @param data pointer to array of data to be sent over i2c.
+ * @param length length of the array.
+ */
 void write_i2c(uint8_t dev_addr, uint8_t *data, uint8_t length);
+
+/**
+ * @brief I2C read function.
+ * 
+ * @param dev_addr target device address.
+ * @param data pointer to array to store the data recieved.
+ * @param length of the data to be recieved.
+ */
 void read_i2c(uint8_t dev_addr, uint8_t *data, uint8_t length);
+
+/**
+ * @brief Set address and read function.
+ * 
+ * @param dev_addr target device address.
+ * @param starting_reg_addr starting address of the EEPROM to read from.
+ * @param data pointer to array to store the data recieved.
+ * @param length of the data to be recieved.
+ */
+void addr_read_i2c(uint8_t dev_addr, uint8_t starting_reg_addr, uint8_t *data, uint8_t length);
 
 #ifdef __cplusplus
 }
