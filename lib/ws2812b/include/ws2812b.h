c5660b95351c0944577c0a7c0fae42be67185e4b
Shierman <165407774+Shierman@users.noreply.github.com>
2025-11-17 22:08:15 -0500

    It should be workin now!
    Yay

--- lib/ws2812b/include/ws2812b.h
+++ lib/ws2812b/include/ws2812b.h
@@ -4,12 +4,13 @@
 /*************************
  * RGB command Constants *
  *************************/
-
 #define HIGH 0x01
 #define LOW 0x00
 #define RESET 0x02
+#define NOP() asm volatile("nop")
 
 #include <avr/io.h>
+#include <util/delay.h>
 #include <stdint.h>
 #include <avr/interrupt.h>
 
@@ -21,30 +22,30 @@
 extern "C" {
 #endif
 
-void PortD0(unsigned char value) {
+static inline void PortD3(unsigned char value) {
     if(value == 0) {
-        PORTD &= 0xFE;
+        PORTD &= ~(1 << PD3);
     } else {
-        PORTD |= 0x01;
+        PORTD |= (1 << PD3);
     }
 }
 
-void Logic1() {
-    PortD0(1);
-    NOP(); NOP(); NOP(); NOP(); NOP(); NOP(); NOP(); NOP(); NOP(); NOP(); NOP(); NOP(); NOP(); NOP(); NOP(); NOP();  // 16xNOP() = 0.8 microsecond delay
-    PortD0(0);
-    NOP(); NOP(); NOP(); NOP(); NOP(); NOP(); NOP(); NOP(); NOP(); // 9xNOP() = 0.45 microsecond delay
+static inline void Logic1() {
+    PortD3(1);
+    NOP(); NOP(); NOP(); NOP(); NOP(); NOP(); NOP(); NOP(); NOP(); NOP(); NOP(); NOP(); NOP();  // 16xNOP() = 0.8 microsecond delay
+    PortD3(0);
+    NOP(); NOP(); NOP(); NOP(); NOP(); NOP(); NOP(); // 9xNOP() = 0.45 microsecond delay
 }
 
-void Logic0() {
-    PortD0(1);
-    NOP(); NOP(); NOP(); NOP(); NOP(); NOP(); NOP(); NOP(); // 8xNOP() = 0.4 microsecond delay
-    PortD0(0);
-    NOP(); NOP(); NOP(); NOP(); NOP(); NOP(); NOP(); NOP(); NOP(); NOP(); NOP(); NOP(); NOP(); NOP(); NOP(); NOP(); NOP(); // 17xNOP() = 0.85 microsecond delay
+static inline void Logic0() {
+    PortD3(1);
+    NOP(); NOP();  // 8xNOP() = 0.4 microsecond delay
+    PortD3(0);
+    NOP(); NOP(); NOP(); NOP(); NOP(); NOP(); NOP(); NOP(); NOP(); NOP(); NOP(); NOP(); NOP(); NOP(); NOP();  // 17xNOP() = 0.85 microsecond delay
 }
 
-void ResetCode() {
-    PortD0(0);
+static inline void ResetCode() {
+    PortD3(0);
     //Delay >= 50 microseconds = 100xNOP()
     NOP(); NOP(); NOP(); NOP(); NOP(); NOP(); NOP(); NOP(); NOP(); NOP(); // 10xNOP() = 5 microsecond delay
     NOP(); NOP(); NOP(); NOP(); NOP(); NOP(); NOP(); NOP(); NOP(); NOP();
@@ -58,7 +59,7 @@ void ResetCode() {
     NOP(); NOP(); NOP(); NOP(); NOP(); NOP(); NOP(); NOP(); NOP(); NOP();
 }
 
-void SendLogic(unsigned char code) {
+static inline void SendLogic(unsigned char code) {
     if(code == HIGH) {
         Logic1();
     } else if(code == LOW) {
@@ -68,41 +69,29 @@ void SendLogic(unsigned char code) {
     }
 }
 
-void SendByte(unsigned char sendByte) {
+static inline void SendByte(unsigned char sendByte) {
     unsigned char bit;
 
-    bit = sendByte >> 7;
-    SendLogic(bit);
-    bit = (sendByte & 0x40) >> 6;
-    SendLogic(bit);
-    bit = (sendByte & 0x20) >> 5;
-    SendLogic(bit);
-    bit = (sendByte & 0x10) >> 4;
-    SendLogic(bit);
-    bit = (sendByte & 0x08) >> 3;
-    SendLogic(bit);
-    bit = (sendByte & 0x04) >> 2;
-    SendLogic(bit);
-    bit = (sendByte & 0x02) >> 1;
-    SendLogic(bit);
-    bit = (sendByte & 0x01);
-    SendLogic(bit);
+    for(char i = 7; i >= 0; i--) {
+        bit = (sendByte >> i) & 0x01; 
+        SendLogic(bit);
+    }
 }
 
-void SendOneRGB(unsigned char R, unsigned char G, unsigned char B) {
+static inline void SendOneRGB(unsigned char R, unsigned char G, unsigned char B) {
     // the standard neopixel LED recieves RGB values in the order B, R, and G
-    SendByte(B);
-    SendByte(R);
     SendByte(G);
+    SendByte(R);
+    SendByte(B);
 }
 
-void SendArrayRGB(unsigned char R[], unsigned char G[], unsigned char B[], unsigned int arraySize) {
+static inline void SendArrayRGB(unsigned char R[], unsigned char G[], unsigned char B[], unsigned int arraySize) {
     unsigned char currR; // these are necessary so as not to modify the array
     unsigned char currG;
     unsigned char currB;
 
     cli(); //Disable interupts
-    for(int i = 0; i < arraySize; i++) { //This only works if the array size is correct
+    for(unsigned int i = 0; i < arraySize; i++) { //This only works if the array size is correct
         currR = R[i];
         currG = G[i];
         currB = B[i];
@@ -117,3 +106,80 @@ void SendArrayRGB(unsigned char R[], unsigned char G[], unsigned char B[], unsig
 #endif
 
 #endif
+
+/* Assumes F_CPU == 16000000UL and PD3 used as output pin */
+
+
+
+// #include <util/delay.h>
+// #include <avr/io.h>
+// #include <avr/interrupt.h>
+// #include <stdint.h>
+
+// static inline void ws2812e_init(void) {
+//     DDRD |= (1 << PD3);   // PD3 output
+//     PORTD &= ~(1 << PD3);
+// }
+
+// /* Reset / latch: WS2812E margin, use 300us */
+// static inline void ws2812e_reset(void) {
+//     PORTD &= ~(1 << PD3);
+//     _delay_us(300);
+// }
+
+// /* Tight, inline byte-send for AVR @16MHz.
+//  * Sends one byte (MSB first). Caller must have disabled interrupts.
+//  */
+// static inline void ws2812e_sendbyte_avr16(uint8_t b) {
+//     volatile uint8_t *port = &PORTD;
+//     uint8_t pinmask = (1 << PD3);
+//     uint8_t hi = *port | pinmask;
+//     uint8_t lo = *port & ~pinmask;
+
+//     for (int8_t i = 7; i >= 0; --i) {
+//         if (b & (1 << i)) {
+//             /* '1' bit: long high (~0.8us) */
+//             *port = hi;
+//             asm volatile(
+//                 "nop\n\t""nop\n\t""nop\n\t""nop\n\t""nop\n\t"
+//                 "nop\n\t""nop\n\t""nop\n\t""nop\n\t""nop\n\t"
+//                 "nop\n\t""nop\n\t""nop\n\t""nop\n\t"
+//             );
+//             *port = lo;
+//             asm volatile("nop\n\t""nop\n\t");
+//         } else {
+//             /* '0' bit: ultra short high (~<300ns), long low */
+//             *port = hi;
+//             /* just one or two NOPs */
+//             asm volatile("nop\n\t""nop\n\t");
+//             *port = lo;
+//             /* long low to make full 1.25us period */
+//             asm volatile(
+//                 "nop\n\t""nop\n\t""nop\n\t""nop\n\t""nop\n\t"
+//                 "nop\n\t""nop\n\t""nop\n\t""nop\n\t""nop\n\t"
+//                 "nop\n\t""nop\n\t""nop\n\t""nop\n\t""nop\n\t"
+//                 "nop\n\t""nop\n\t""nop\n\t""nop\n\t""nop\n\t"
+//                 "nop\n\t"
+//             );
+//         }
+//     }
+// }
+
+
+// /* Send a pixel in GRB order */
+// static inline void ws2812e_send_pixel_avr16(uint8_t r, uint8_t g, uint8_t b) {
+//     /* caller must have interrupts disabled */
+//     ws2812e_sendbyte_avr16(g);
+//     ws2812e_sendbyte_avr16(r);
+//     ws2812e_sendbyte_avr16(b);
+// }
+
+// /* Send arrays: R[], G[], B[], count */
+// static inline void ws2812e_send_array_avr16(uint8_t R[], uint8_t G[], uint8_t B[], uint16_t count) {
+//     cli(); /* disable interrupts for timing */
+//     for (uint16_t i = 0; i < count; ++i) {
+//         ws2812e_send_pixel_avr16(R[i], G[i], B[i]);
+//     }
+//     ws2812e_reset();
+//     sei();
+// }
--- src/main.cpp
+++ src/main.cpp
@@ -3,29 +3,57 @@
 #include <atmega328p_i2c.h>
 #include <adxl345.h>
 #include <mcp4725.h>
+#include <ws2812b.h>
+#define F_CPU 16000000UL
+#include <util/delay.h>
 
 // Accelerometer Interupt
-uint8_t a_flag = 0;
+//uint8_t a_flag = 0;
+volatile uint8_t a_flag = 0; //HEY RYAN, I CHANGED THIS... MAYBE ITS HELPFUL?
 ISR(INT0_vect) {
     a_flag = 1;
 }
 
 int main() {
-    // Initalize I2C
-    start_i2c();
+    DDRD |= (1 << PD3); //Pin D3 is output
+    
+unsigned char R[60] = {
+255,255,255,255,255,255,255,255,255,255,
+255,230,204,178,153,128,102,77,51,26,
+0,0,0,0,0,0,0,0,0,0,
+0,0,0,0,0,0,0,0,0,0,
+0,25,51,76,102,128,153,179,204,230,
+255,255,255,255,255,255,255,255,255,255
+};
 
-    // Setup accelerometer
-    if (is_adxl345_present() != 1) return -1;
-    setup_adxl345_activity_interrupt();
-    enable_adxl345();
-    write_mcp4725(0x0000);
+unsigned char G[60] = {
+0,25,51,77,102,128,153,178,204,229,
+255,255,255,255,255,255,255,255,255,255,
+255,255,255,255,255,255,255,255,255,255,
+255,230,204,178,153,128,102,76,51,25,
+0,0,0,0,0,0,0,0,0,0,
+0,0,0,0,0,0,0,0,0,0
+};
 
+unsigned char B[60] = {
+0,0,0,0,0,0,0,0,0,0,
+0,0,0,0,0,0,0,0,0,0,
+0,25,51,76,102,128,153,179,204,229,
+255,255,255,255,255,255,255,255,255,255,
+255,255,255,255,255,255,255,255,255,255,
+255,230,204,179,153,128,102,77,51,26
+};
+
+
+    unsigned char black[60] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    unsigned char white[60] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
+    unsigned char one[1] = {0xFF};
+    unsigned char zero[1] = {0};
     while (1) {
-        // do stuff
-        if (a_flag) {
-            acceleration fdata = get_current_acceleration();
-            // do something with data
-            a_flag = 0;
-        }
+
+        SendArrayRGB(R, G, B, 60);
+        _delay_ms(500);
+
     }
 }
+
