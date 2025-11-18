ee5f4a24abf49baaee9e94e22b5549c1e9bf7bc4
builderdev212 <builder@builderdev.tech>
2025-11-18 09:25:45 -0500

    finalize audio

--- include/audio_header_gen.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-for f in $(find $(dirname "$0") -name "*.wav"); do
-    echo "$f"
-    python $(dirname "$0")/../wav_to_h/converter.py $f
-done
--- src/main.cpp
+++ src/main.cpp
@@ -2,56 +2,45 @@
 #include <avr/interrupt.h>
 #include <util/delay.h>
 #include <stdint.h>
+
 #include <atmega328p/i2c.h>
 #include <atmega328p/serial.h>
 #include <mpu6050.h>
-#include <mcp4725.h>
-
-// Sound files
-#include "swing3.h"
-#define SWING3_LEN 7746
+#include <PWMaudio.h>
 
 // Accelerometer Interupt
-uint8_t a_flag = 0;
-uint16_t i = 0;
+volatile uint8_t a_flag = 0;
 ISR(INT0_vect) {
     a_flag = 1;
-    start_timer1();
-    i = 0;
-}
-
-// WAV Timing Interrupt
-uint8_t sready = 0;
-ISR(TIMER1_COMPA_vect) {
-    sready = 1;
 }
 
 int main() {
     // Initialize hardware
     uart_init(9600);
     start_i2c();
-    setup_mpu6050(5, 1);
-    setup_timer1_4khz();
-    write_mcp4725(0x000);
+    setup_mpu6050(2, 1);
+    setupPWM();
+    setFrequency(100);
 
     // enable global interrupts
     sei();
 
     while (1) {
-        // if the timer is polled, play current audio
-        if (sready) {
-            sready = 0;
-            write_mcp4725((uint16_t)swing3[i] << 4);
-            i++;
-        }
         // play sound if interrupt has been recieved
         if (a_flag) {
-            if (i == SWING3_LEN) {
-                clear_mpu6050_int_status();
-                stop_timer1();
-                a_flag = 0;
-                i = 0;
+            clear_mpu6050_int_status();
+            for (int  i = 100; i > 50; i--) {
+                setFrequency(i);
+                _delay_ms(20);
             }
+            for (int  i = 50; i < 101; i++) {
+                setFrequency(i);
+                _delay_ms(10);
+            }
+            setFrequency(100);
+            a_flag = 0;
         }
     }
+
+    return 0;
 }
