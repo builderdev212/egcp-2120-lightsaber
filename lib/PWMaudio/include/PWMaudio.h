a76080fc4db335e8fa204d55c3f0caaf623d5216
builderdev212 <builder@builderdev.tech>
2025-11-19 09:01:14 -0500

    add noise work

--- lib/PWMaudio/include/PWMaudio.h
+++ lib/PWMaudio/include/PWMaudio.h
@@ -2,6 +2,8 @@
 #define PWMAUDIO_H
 
 #include <avr/io.h>
+#include <avr/pgmspace.h>
+#include <avr/interrupt.h>
 #include <stdlib.h>
 
 /***********************
@@ -11,6 +13,12 @@
 #define PWM_PORT PORTB
 #define PWM_PORT_IO_CONF DDRB
 #define PWM_PIN 0x02
+
+#ifndef F_CPU
+#define F_CPU 16000000UL
+#endif
+
+#define SAMPLE_RATE 8000UL
 
 /***************************************
  * PWM Audio User Function Definitions *
@@ -22,7 +30,7 @@ extern "C" {
 
 void setupPWM();
 
-void setFrequency(uint16_t freq_hz);
+void trigger_slash(uint16_t ms);
 
 #ifdef __cplusplus
 }
